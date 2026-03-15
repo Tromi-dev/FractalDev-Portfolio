3e55b1622a78572bf79359ff5a0fca6ff1c0d465
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2026-03-15 18:19:21 +0000

    Add Flask app structure with routing and templates; include CSS and JS files for styling and functionality

---
+++ app.py
@@ -0,0 +1,78 @@
+from main import *
+from flask import Flask, render_template
+import markdown
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+@app.route("/landing")
+def landing():
+    links = set_active_link("landing")
+
+    return render_template("index.html", links=links)
+
+
+@app.route("/about")
+def about():
+    links = set_active_link("about")
+
+    return render_template("about.html", links=links)
+
+
+@app.route("/projects")
+def projects():
+    links = set_active_link("projects")
+
+    repos = reformat(get_github_repos())
+    return render_template("projects.html", repos=repos, links=links)
+
+
+@app.route("/projects/<name>")
+def project_page(name):
+
+    repos = reformat(get_github_repos())
+    repo_list = list_repo(repos)
+
+    if name in repo_list:
+        readme_path = f"static/github_cache/{name}/README.md"
+        with open(readme_path, "r") as readme_file:
+            content = readme_file.read()
+
+        html_content = markdown.markdown(content)
+
+        links = set_active_link("projects")
+
+        return render_template(
+            "project.html",
+            content=html_content,
+            repo_data=get_repo(repos, name),
+            links=links,
+        )
+    else:
+        return "not found"
+
+
+@app.route("/contact")
+def contact():
+    links = set_active_link("contact")
+
+    return render_template("contact.html", links=links)
+
+
+@app.route("/hobbies")
+def hobbies():
+    links = set_active_link("hobbies")
+
+    # * Move to client
+    # uptime = get_uptime()
+    # temp = get_temp()
+    # cpu_usage = psutil.cpu_percent()
+
+    return render_template("hosting.html", links=links)
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import *
 import requests
 import subprocess
 import os
-import markdown
 import diskcache as dc
 import psutil
 import datetime
@@ -10,20 +8,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-API_URL = os.getenv('API_URL')
-USERNAME = os.getenv('GITHUB_USERNAME')
+API_URL = os.getenv("API_URL")
+USERNAME = os.getenv("GITHUB_USERNAME")
 
 
 # Path to store the repositories locally
-BASE_DIR = os.getenv('BASE_DIR')
+BASE_DIR = os.getenv("BASE_DIR")
 
-cache = dc.Cache('cache_directory')
+cache = dc.Cache("cache_directory")
+
 
 def get_uptime():
     try:
-        with open('/proc/uptime', 'r') as f:
+        with open("/proc/uptime", "r") as f:
             uptime_seconds = float(f.readline().split()[0])
 
         timedelta = datetime.timedelta(seconds=uptime_seconds)
@@ -54,11 +53,6 @@ def get_temp():
         return "Temperature Unknown"
 
 
-
-
-
-
-
 # Function to clone or update repositories
 def clone_or_update_repo(repo_url, repo_name):
     repo_path = os.path.join(BASE_DIR, repo_name)
@@ -66,17 +60,17 @@ def clone_or_update_repo(repo_url, repo_name):
     # If the repo directory already exists, pull the latest changes
     if os.path.exists(repo_path):
         print(f"Updating repository: {repo_name}")
-        subprocess.run(['git', '-C', repo_path, 'pull'], check=True)
+        subprocess.run(["git", "-C", repo_path, "pull"], check=True)
     else:
         # Clone the repository
         print(f"Cloning repository: {repo_name}")
-        subprocess.run(['git', 'clone', repo_url, repo_path], check=True)
+        subprocess.run(["git", "clone", repo_url, repo_path], check=True)
 
 
 # Fetch the list of repositories from GitHub API
 def get_github_repos():
 
-    TOKEN = os.getenv('TOKEN')
+    TOKEN = os.getenv("TOKEN")
 
     # url = f"http://api.github.com/users/{USERNAME}/repos"
 
@@ -87,16 +81,14 @@ def get_github_repos():
         return cached_data
 
     # Set the headers for authentication
-    headers = {
-        'Authorization': f'token {TOKEN}'
-    }
+    headers = {"Authorization": f"token {TOKEN}"}
 
     response = requests.get(API_URL, headers=headers)
 
     # Check if the response is successful
     if response.status_code == 200:
         data = response.json()
-        sorted_repos = sorted(data, key=lambda x: x['updated_at'], reverse=True)
+        sorted_repos = sorted(data, key=lambda x: x["updated_at"], reverse=True)
 
         # Store the data in the cache with an expiration time of 24 hours
         cache.set(USERNAME, sorted_repos, expire=3600)  # 24 hours
@@ -115,8 +107,8 @@ def main():
     repos = get_github_repos()
 
     for repo in repos:
-        repo_name = repo['name']
-        repo_url = repo['clone_url']
+        repo_name = repo["name"]
+        repo_url = repo["clone_url"]
         clone_or_update_repo(repo_url, repo_name)
 
 
@@ -125,41 +117,40 @@ def get_repos(username):
     response = requests.get(url)
     data = response.json()
 
-    sorted_repos = sorted(data, key=lambda x: x['updated_at'], reverse=True)
+    sorted_repos = sorted(data, key=lambda x: x["updated_at"], reverse=True)
 
     return sorted_repos
+
 
 def update():
     un_repos = get_repos(USERNAME)
 
     repos = reformat(un_repos)
 
-
     if not os.path.exists(BASE_DIR):
         os.makedirs(BASE_DIR)
 
-
-
     for repo in repos:
-        repo_name = repo['name']
-        repo_url = repo['clone_url']
+        repo_name = repo["name"]
+        repo_url = repo["clone_url"]
         clone_or_update_repo(repo_url, repo_name)
 
 
 def get_repo(repos, name):
     for repo in repos:
-        repo_name = repo['name']
+        repo_name = repo["name"]
         if repo_name == name:
             return repo
 
+
 def reformat(repos):
     for repo in repos:
-        repo['local_url'] = "github_cache/" + repo['name']
-        repo['page_url'] = "/projects/" + repo['name']
-        repo["watchers"] = str(repo['watchers'])
-        repo["forks_count"] = str(repo['forks_count'])
+        repo["local_url"] = "github_cache/" + repo["name"]
+        repo["page_url"] = "/projects/" + repo["name"]
+        repo["watchers"] = str(repo["watchers"])
+        repo["forks_count"] = str(repo["forks_count"])
 
-        print(repo['local_url'])
+        print(repo["local_url"])
 
     return repos
 
@@ -169,81 +160,19 @@ def list_repo(repos):
     for repo in repos:
         repo_list.append(repo["name"])
 
-
     return repo_list
-
-
 
 
 def set_active_link(active_route):
     links = {
-        'landing': {'name': 'landing', 'active': False},
-        'about': {'name': 'about', 'active': False},
-        'projects': {'name': 'projects', 'active': False},
-        'contact': {'name': 'contact', 'active': False},
-        'webhosting': {'name': 'webhosting', 'active': False},
+        "landing": {"name": "landing", "active": False},
+        "about": {"name": "about", "active": False},
+        "projects": {"name": "projects", "active": False},
+        "contact": {"name": "contact", "active": False},
+        "hobbies": {"name": "hobbies", "active": False},
     }
-    links[active_route].active = True
+
+    links[active_route].update({"active": True})
+    # for i in links:
+    #     print(f"\n{i['name']}")
     return links
-    
-
-@app.route('/')
-@app.route('/home')
-@app.route('/landing')
-def landing():
-    links = set_active_link('landing')
-
-    return render_template("landing.html", links=links)
-
-@app.route('/about')
-def about():
-    links = set_active_link('about')
-
-    return render_template("about.html", links=links)
-
-@app.route('/projects')
-def projects():
-    links = set_active_link('projects')
-
-    repos = reformat(get_github_repos())
-    return render_template("projects.html", repos=repos, links=links)
-
-@app.route('/projects/<name>')
-def project_page(name):
-
-    repos = reformat(get_github_repos())
-    repo_list = list_repo(repos)
-
-    if name in repo_list:
-        readme_path = f'static/github_cache/{name}/README.md'
-        with open(readme_path, 'r') as readme_file:
-            content = readme_file.read()
-
-        html_content = markdown.markdown(content)
-
-        links = set_active_link('projects')
-
-        return render_template('project.html', content=html_content, repo_data=get_repo(repos, name), links=links)
-    else:
-        return "not found"
-
-@app.route('/contact')
-def contact():
-    links = set_active_link('contact')
-
-    return render_template("contact.html", links=links)
-
-@app.route('/webhosting')
-def host():
-    links = set_active_link('webhosting')
-
-    uptime = get_uptime()
-    temp = get_temp()
-    cpu_usage = psutil.cpu_percent()
-    
-    return render_template("hosting.html", uptime=uptime, temp=temp, cpu_usage=cpu_usage, links=links)
-
-
-if __name__ == '__main__':
-
-    app.run()
