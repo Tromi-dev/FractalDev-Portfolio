a1e25b7d7c21b5df75854cb56d197adb75197d5c
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2026-04-07 16:54:14 +0100

    Refactor navigation components, enhance mobile homepage styles, and improve footer layout
    reverted back to the no-js sidebar, and added a codition to make sidebar hidden before fully initializing

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
-(() => {
-  const drawer = document.getElementById("navSidebar");
-  const openButton = document.getElementById("navSidebarOpenButton");
+// (() => {
+//   const drawer = document.getElementById("navSidebar");
+//   const openButton = document.getElementById("navSidebarOpenButton");
 
-  openButton.addEventListener("click", () =>
-    drawer.setAttribute("open", "true"),
-  );
-})();
+//   openButton.addEventListener("click", () =>
+//     drawer.setAttribute("open", "true"),
+//   );
+// })();
