65a6117e52a4622c214b28446c9364001b032213
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2026-04-24 20:50:43 +0100

    all parts of hamburger now open side menu

--- static/script.js
+++ static/script.js
@@ -4,6 +4,20 @@
   console.log(video.playbackRate + "x speed");
 })();
 
+(() => {
+  if (!window.matchMedia("(max-width: 1230px)").matches) return;
+
+  const mobileMenu = document.querySelector(".nav-items.mobile");
+  const hamburger = document.querySelector("#navSidebarOpenButton");
+
+  mobileMenu?.addEventListener("click", e => {
+    // Prevent infinite loop: ignore clicks that came from the hamburger itself
+    if (hamburger?.contains(e.target)) return;
+
+    hamburger?.click();
+  });
+})();
+
 async function getServerStats() {
   const req = await fetch(/* server route */);
 }
